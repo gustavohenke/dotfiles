b6b7529522c779848a8598e5d036c00e3e410542
Gustavo Henke <guhenke@gmail.com>
2026-04-27 18:45:53 +1000

    Add banner to all scripts

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 set -eu
 
+echo
+echo
+echo "::: Running dotfiles installation"
+
 # Install to current user's local bins only
 sh -c "$(curl -fsLS get.chezmoi.io)" -- -b $HOME/.local/bin init --apply gustavohenke
